142830538a29ebd91243636e1e5aca0edbd62ec9
Kim, Sungchul <sungchul.kim@intel.com>
2023-08-11 16:04:00 +0900

    Fix visual prompter

--- src/otx/algorithms/visual_prompting/adapters/torch/models/visual_prompters/visual_prompter.py
+++ src/otx/algorithms/visual_prompting/adapters/torch/models/visual_prompters/visual_prompter.py
@@ -5,12 +5,8 @@
 #
 
 
-def build_prompter(type: str):
-    if type.lower() == "sam":
-        from otx.algorithms.visual_prompting.adapters.torch.models.visual_prompters.sam import SAM
-        return SAM
-
-
 class VisualPrompter:
     def __new__(cls, type: str, *args, **kwargs):
-        return build_prompter(type)(*args, **kwargs)
+        if type.lower() == "sam":
+            from otx.algorithms.visual_prompting.adapters.torch.models.visual_prompters.sam import SAM
+            return SAM(*args, **kwargs)
